7ddb2f98a206a4e31ed67b382b09e2b6f66cf949
Falling10fruit <budiyoga09nur@gmail.com>
2023-05-05 12:47:09 +0700

    manuallyPlaceEverythingInPlaceOhMyGoshHowDoesCSSWork

--- Assets/TokoProgram2/index.js
+++ Assets/TokoProgram2/index.js
@@ -2,7 +2,7 @@ var Tab = document.getElementsByClassName("Tab");
 var Add = document.getElementsByClassName("Add");
 var Left = document.getElementsByClassName("Left");
 var Right = document.getElementsByClassName("Right");
-var TableRow = document.getElementsByClassName("TableRow");
+var RecieptRow = document.getElementsByClassName("RecieptRow");
 var SubTotal = document.getElementsByClassName("SubTotal");
 var Close = document.getElementsByClassName("Close");
 var Clear = document.getElementById("Clear");
@@ -14,36 +14,10 @@ var Example = document.getElementById("Example");
 var Total = document.getElementById("Total");
 var TabMode = 0;
 
-document.body.style.height = window.innerHeight + "px";
-Cashier.style.height = DataBase.style.height = window.innerHeight - Tabs.getBoundingClientRect().height + "px";
-Cashier.style.top = DataBase.style.top = Tabs.getBoundingClientRect().height + "px";
-Cashier.style.width = DataBase.style.width = window.innerWidth + "px";
-DataBase.style.left = Cashier.getBoundingClientRect().width + "px";
-Total.style.width = window.innerWidth - 100 + "px";
-Total.style.top = window.innerHeight - Total.getBoundingClientRect().height + "px";
-for (let i = 0; i < 2; i++) {
-    Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().right + "px";
-    Right[i].style.left = Left[i].getBoundingClientRect().right + "px";
-    Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().width + "px";
-    Add[i].style.width = Add[i].getBoundingClientRect().height + "px";
-    Clear.style.width = Clear.getBoundingClientRect().height + "px";
-}
+manuallyPlaceEverythingInPlaceOhMyGoshHowDoesCSSWork();
 
 window.addEventListener("resize", function () {
-    document.body.style.height = window.innerHeight + "px";
-    Cashier.style.height = DataBase.style.height = window.innerHeight - Tabs.getBoundingClientRect().height + "px";
-    Cashier.style.top = DataBase.style.top = Tabs.getBoundingClientRect().height + "px";
-    Cashier.style.width = DataBase.style.width = window.innerWidth + "px";
-    DataBase.style.left = Cashier.getBoundingClientRect().width + "px";
-    Total.style.width = window.innerWidth - 100 + "px";
-    Total.style.top = window.innerHeight - Total.getBoundingClientRect().height + "px";
-    for (let i = 0; i < 2; i++) {
-        Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().right + "px";
-        Right[i].style.left = Left[i].getBoundingClientRect().right + "px";
-        Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().width + "px";
-        Add[i].style.width = Add[i].getBoundingClientRect().height + "px";
-        Clear.style.width = Clear.getBoundingClientRect().height + "px";
-    }
+    manuallyPlaceEverythingInPlaceOhMyGoshHowDoesCSSWork();
 });
 
 Tab[0].addEventListener("mouseover", function (e) {
@@ -93,24 +67,22 @@ for (var i = 0; i < TableRow.length; i++) {
     }
 }
 
-Add[0].addEventListener("mouseover", function () {
-    Add[0].style.backgroundColor = "rgb(100, 100, 100)";
-});
-
-Add[0].addEventListener("mouseout", function () {
-    Add[0].style.backgroundColor = "rgb(80, 80, 80)";
-});
-
-Add[0].addEventListener("mouseup", function () {
-    Add[0].style.backgroundColor = "rgb(100, 100, 100)";
-})
+addButtonStyleThing(Add[0]);
+addButtonStyleThing(Add[1]);
+addButtonStyleThing(Clear);
 
 Add[0].addEventListener("mousedown", function () {
-    Add[0].style.backgroundColor = "rgb(70, 70, 70)";
-
     var NewItem = Example.cloneNode(true);
     NewItem.removeAttribute("id");
     Items.appendChild(NewItem);
+
+    Close[Close.length - 1].addEventListener("click", function () {
+        NewItem.remove();
+    });
+});
+
+Clear.addEventListener("mousedown", function () {
+    for ()
 });
 
 Tick();
@@ -124,3 +96,38 @@ function Tick () {
 
     requestAnimationFrame(Tick);
 }
+
+function manuallyPlaceEverythingInPlaceOhMyGoshHowDoesCSSWork () {
+    document.body.style.height = window.innerHeight + "px";
+    Cashier.style.height = DataBase.style.height = window.innerHeight - Tabs.getBoundingClientRect().height + "px";
+    Cashier.style.top = DataBase.style.top = Tabs.getBoundingClientRect().height + "px";
+    Cashier.style.width = DataBase.style.width = window.innerWidth + "px";
+    DataBase.style.left = Cashier.getBoundingClientRect().width + "px";
+    Total.style.width = window.innerWidth - 100 + "px";
+    Total.style.top = window.innerHeight - Total.getBoundingClientRect().height + "px";
+    Clear.style.width = Clear.getBoundingClientRect().height + "px";
+    for (let i = 0; i < 2; i++) {
+        Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().right + "px";
+        Right[i].style.left = Left[i].getBoundingClientRect().right + "px";
+        Right[i].style.width = window.innerWidth - Left[i].getBoundingClientRect().width + "px";
+        Add[i].style.width = Add[i].getBoundingClientRect().height + "px";
+    }
+}
+
+function addButtonStyleThing (Element) {
+    Element.addEventListener("mouseover", function () {
+        Element.style.backgroundColor = "rgb(100, 100, 100)";
+    });
+    
+    Element.addEventListener("mouseout", function () {
+        Element.style.backgroundColor = "rgb(80, 80, 80)";
+    });
+    
+    Element.addEventListener("mouseup", function () {
+        Element.style.backgroundColor = "rgb(100, 100, 100)";
+    });
+
+    Element.addEventListener("mousedown", function () {
+        Element.style.backgroundColor = "rgb(70, 70, 70)";
+    });
+};
