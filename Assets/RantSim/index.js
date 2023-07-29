32e4aa0f48ab3979e99e9e0ddc91f827837d5b46
Falling10fruit <budiyoga09nur@gmail.com>
2023-07-29 23:15:56 +0700

    Delciious

--- Assets/RantSim/index.js
+++ Assets/RantSim/index.js
@@ -17,20 +17,34 @@ var Creatures = {
 var Traits = ["OpposableThumbs", "Tools", "Lungs", "ToughSkin", "ToughStomach", "StrongArms", "ShortTermMemory", "Fur", "Communication", "Tail", "Gills", "WebbedFeet", "Scales", "GrassStomach", "Social", "Wings", "Echolocation", "Beaks", "Claws", "Mane", "NoLimbs", "Fins"];
 var PrimaryTraits = [0, 1, 2, 3, 7, 12];
 var TargetTraits = [0, 1, 2, 3, 4, 5, 6]
+var StartingPopulation = 2;
+var KidAmount = 3;
 var TraitLimit = 8;
-var SelectPrimaryTraits = document.getElementById("SelectPrimaryTraits");
-var SelectTargetTraits = document.getElementById("SelectTargetTraits");
-var SelectTraitLimit = document.getElementById("SelectTraitLimit");
 var SelectStartingPopulation = document.getElementById("SelectStartingPopulation");
 var SelectKidAmount = document.getElementById("SelectKidAmount");
+var SelectTraitLimit = document.getElementById("SelectTraitLimit");
+var SelectPrimaryTraits = document.getElementById("SelectPrimaryTraits");
+var SelectTargetTraits = document.getElementById("SelectTargetTraits");
 var PrimaryTraitOptions = document.getElementsByClassName("PrimaryTraitOptions");
-var TargetTraitOptions = document.getElementsByClassName("PrimaryTraitOptions");
+var TargetTraitOptions = document.getElementsByClassName("TargetTraitOptions");
+
+SelectStartingPopulation.addEventListener("")
+SelectKidAmount.addEventListener("")
+SelectTraitLimit.addEventListener("")
+
+ResetTraitOptions();
 
 function ResetTraitOptions () {
     let TraitOption = document.createElement("input");
     TraitOption.setAttribute("type", "checkbox");
 
     for (var i = 0; i < Traits.length; i++) {
+        if (i != 0) {
+            let BR = document.createElement("br");
+            SelectTargetTraits.appendChild(BR.cloneNode(true));
+            SelectPrimaryTraits.appendChild(BR.cloneNode(true));
+        }
+
         let PrimaryTraitOption = TraitOption.cloneNode(true);
         PrimaryTraitOption.setAttribute("class", "PrimaryTraitOptions");
         for (var x = 0; x < PrimaryTraits.length; x++) {
@@ -48,5 +62,28 @@ function ResetTraitOptions () {
             }
         }
         SelectTargetTraits.appendChild(TargetTraitOption);
+
+        let Label = document.createElement("label");
+        Label.innerHTML = Traits[i];
+        SelectPrimaryTraits.appendChild(Label.cloneNode(true));
+        SelectTargetTraits.appendChild(Label.cloneNode(true));
+    }
+
+    for (var i = 0; i < PrimaryTraitOptions.length; i++) {
+        PrimaryTraitOptions[i].addEventListener("click", function () {
+            if (!PrimaryTraitOptions[i].selected) {
+                PrimaryTraits.splice(0, 1);
+                PrimaryTraits.push(i);
+            }
+        })
+    }
+
+    for (var i = 0; i < TargetTraitOptions.length; i++) {
+        TargetTraitOptions[i].addEventListener("click", function () {
+            if (!TargetTraitOptions[i].selected) {
+                TargetTraits.splice(0, 1);
+                TargetTraits.push(i);
+            }
+        })
     }
 }
